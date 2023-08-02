432b72af9e9dea26f14fa9195e6d9c5c56d8fad6
sowndaryas-ca21 <sowndaryas.ca21@kascsathy.ac.in>
2023-08-02 10:29:06 +0000

    first commit

---
+++ c/matrix.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+        int a,b;
+        printf("Enter two integer values seperated by space :");
+        scanf("%d %d",&a,&b);
+        printf("The value of %d + %d = %d \n", a,b,a+b);
+        printf("The value of %d * %d = %d \n", a,b,a*b);
+        return 0;
+}
